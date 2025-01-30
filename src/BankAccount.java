d81731bdbdbeb6ab7ba9ad58e56957ecf0bbdd1f
enrqh <enriquehern2505@gmail.com>
2025-01-30 21:39:09 +0000

    First commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,18 @@
+public class BankAccount{
+    public static void main (String[] args){
 
+        float bankAccount = 2175.37f;
+        bankAccount -= 302.50; // Withdraw
+        bankAccount += 50.03; // Deposit
+        bankAccount -= bankAccount/2; // Withdraws half of current balance
+        bankAccount += 20; // Deposit
+        bankAccount --; // Withdraw $1
+        bankAccount += bankAccount; // Deposit that doubles current balance 
+        bankAccount ++; // Deposit $1
+
+        System.out.printf("%.2f", bankAccount); // Outputs float rounded to hundreth place
+
+
+
+    }   
+}
